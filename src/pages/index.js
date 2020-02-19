7774f2eb3f0c48e6f6fbed6497c6aaaf58e5fd73
Mathew Leland <mathew.leland@clearme.com>
2020-02-19 11:09:47 -0500

    just saying hi to james

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = props => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi guys</h1>
+      <h1>Hi James</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
